708414affd116c4c508f73cb37a50fa03ff424d0
Shaw <shawsong2022@gmail.com>
2023-02-03 22:32:59 -0800

    finish

---
+++ manager.js
@@ -0,0 +1,45 @@
+const Employee = require("./employee");
+
+class Manager extends Employee {
+    constructor(name, salary, title, manager = null, employees = []) {
+        super(name, salary, title, manager);
+        this.employees = employees;
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+
+    calculateBonus(multiplier) {
+        return (this.salary + this._totalSubSalary()) * multiplier;
+    }
+
+    _totalSubSalary() {
+        let sum = 0;
+        for (let employee of this.employees) {
+            sum += employee.salary;
+            if (employee instanceof Manager) {
+                sum += employee._totalSubSalary();
+            }
+        }
+        return sum;
+
+    }
+}
+
+// const splinter = new Manager('Splinter', 100000, 'Sensei');
+// const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+// const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+// const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+// const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
+
+// console.log(splinter.calculateBonus(0.05)); // => 22500
+// console.log(leo.calculateBonus(0.05)); // => 17500
+// console.log(raph.calculateBonus(0.05)); // => 13000
+
+
+try {
+    module.exports = Manager;
+} catch {
+    module.exports = null;
+}
---
+++ test.js
@@ -0,0 +1,32 @@
+const Employee = require('./employee');
+const Manager = require('./manager');
+
+const hobbes = new Manager('Hobbes', 1000000, 'Founder');
+const calvin = new Manager('Calvin', 130000, 'Director', hobbes);
+const susie = new Manager('Susie', 100000, "TA Manager", calvin);
+const lily = new Employee("Lily", 90000, "TA", susie);
+const clifford = new Employee('Clifford', 90000, "TA", susie);
+console.log(hobbes.calculateBonus(0.05));
+console.log(calvin.calculateBonus(0.05));
+console.log(susie.calculateBonus(0.05));
+console.log(lily.calculateBonus(0.05));
+console.log(clifford.calculateBonus(0.05));
+
+
+// | Name     | Salary  | Title      | Boss   |
+// | -------- | ------- | ---------- | ------ |
+// | Hobbes   | 1000000 | Founder    | null   |
+// | Calvin   | 130000  | Director   | Hobbes |
+// | Susie    | 100000  | TA Manager | Calvin |
+// | Lily     | 90000   | TA         | Susie  |
+// | Clifford | 90000   | TA         | Susie  |
+
+// If Hobbes gets a bonus multiplier of 0.05, their bonus will be $70,500.
+
+// If Calvin gets a bonus multiplier of 0.05, their bonus will be $20,500.
+
+// If Susie gets a bonus multiplier of 0.05, their bonus will be $14,000.
+
+// If Lily gets a bonus multiplier of 0.05, their bonus will be $4,500.
+
+// If Clifford gets a bonus multiplier of 0.05, their bonus will be $4,500.
